29c79adcfddb4e8548aa970c34eefe4b3ecabd6d
joao pedro rodrigues <pedro.rodrigues.joao1407@escola.pr.gov.br>
2026-06-15 12:16:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function insert (num) {
+    var numero = document. getElementById ('resultado'). innerHTML;
+    document. getElementById ('resultado'). innerHTML=numero+num;
+    }
+function clean (num){
+    document. getElementById ('resultado'). innerHTML="";
+}
+function back (){
+    var numero = document. getElementById ('resultado'). innerHTML;
+    document getElementById ('resultado'). innerHTML=reaultado. substring (0,resultado.length-1);
+}
